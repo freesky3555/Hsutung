a69e8fd565d84d7bc957b66ba2631c7ac9befead
freesky3555 <freesky3555@gmail.com>
2021-01-19 22:27:26 +0800

    font and Chinese
    little bit change

--- gittest/Helloworld.py
+++ gittest/Helloworld.py
@@ -17,9 +17,9 @@ def typeit(widget, index, string):
          widget.after(250, typeit, widget, index, string[1:])
 
 root = tk.Tk()
-text = tk.Text(root, width=40, height=8)
+text = tk.Text(root, width=40, height=8, font=("Arial",25))
 text.pack(fill="both", expand=True)
-typeit(text, "1.0", "Hello, World!\nHello, 2021!\nHello, GitHub!\nThis is a greeting from City YUCI!")
+typeit(text, "1.0", "Hello, World!\nHello, 2021!\nHello, GitHub!\nThis is a greeting from CITY YuCi(榆次) !!")
 
 root.mainloop()
 #############################################################
