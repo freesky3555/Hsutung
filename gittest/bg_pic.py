cf727b3361b977fe1d1e8c431f588310716b8519
freesky3555 <freesky3555@gmail.com>
2021-01-21 05:48:10 +0800

    The branch for dev

---
+++ gittest/bg_pic.py
@@ -0,0 +1,7 @@
+root = Tk()
+frame = Frame(root)
+image = PhotoImage(file='c:/users/xxxxxxx/desktop/source/pics/hitta.gif')
+button = Button(frame, image=image)
+button.pack()
+frame.pack()
+root.mainloop()
